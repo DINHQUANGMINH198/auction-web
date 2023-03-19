8a1baf8f819445394ec9d5f8aaa1457ca5f9ce50
DINHQUANGMINH198 <dinhquangminh198@gmail.com>
2023-03-19 15:14:07 +0700

    change footer

--- frontend/src/components/partials/Footer.js
+++ frontend/src/components/partials/Footer.js
@@ -7,10 +7,10 @@ const Footer = () => {
     <Container className="mt-1 text-center">
       <Row>        
         <div className="mx-auto pt-3">
-          <p className="mb-0 font-weight-bold">&copy; 2023 MINHDQ</p>
+          <p className="mb-0 font-weight-bold">&copy; 2023 DATNT</p>
           <hr className="my-0 bg-secondary"/>
-          <p className="mb-0">email: minhdq198@gmail.com</p>
-          <p>https://github.com/DINHQUANGMINH198</p>
+          <p className="mb-0">email: btl.vhvl2021@gmail.com</p>
+          <p>https://github.com/AUCTION</p>
         </div>
       </Row>      
     </Container>
